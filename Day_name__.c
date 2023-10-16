9ae59a25f47f5aa78aa24eef0e4b70aaa1b27e79
sivapranaydakamari <147487910+sivapranaydakamari@users.noreply.github.com>
2023-10-16 15:28:48 +0530

    #include<stdio.h> int main() {     char day;     scanf("%c",&day);     if(day == '1')     {         printf("Monday");     }     else if(day == '2')     {         printf("Tuesday");     }     else if(day == '3')     {         printf("Wednesday");     }     else if(day == '4')     {         printf("Thursday");     }     else if(day == '5')     {         printf("Friday");     }     else if(day == '6')     {         printf("Saturday");     }     else     {         printf("Invalid input");     } }

--- Day_name__.c
+++ Day_name__.c
@@ -1,29 +1,29 @@
 #include<stdio.h>
 int main()
 {
-    int day;
-    scanf("%d",&day);
-    if (day==1)
+    char day;
+    scanf("%c",&day);
+    if(day == '1')
     {
         printf("Monday");
     }
-    else if(day==2)
+    else if(day == '2')
     {
         printf("Tuesday");
     }
-    else if (day==3)
+    else if(day == '3')
     {
         printf("Wednesday");
     }
-    else if (day==4)
+    else if(day == '4')
     {
         printf("Thursday");
     }
-    else if (day==5)
+    else if(day == '5')
     {
         printf("Friday");
     }
-    else if (day==6)
+    else if(day == '6')
     {
         printf("Saturday");
     }
