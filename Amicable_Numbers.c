76482e97acc99d90a2a47b4f2c2b7f05840b206c
sivapranaydakamari <147487910+sivapranaydakamari@users.noreply.github.com>
2023-11-02 21:21:17 +0530

    #include<stdio.h> int main() {     int n,m,i,sum1=0,sum2=0;     scanf("%d%d",&n,&m);     for(i=1;i<n;i++)     {         if(n%i==0)         {             sum1=sum1+i;         }     }     for(i=1;i<m;i++)     {         if(m%i==0)         {             sum2=sum2+i;         }     }     if(sum1==m && sum2==n)     {         printf("Amicable");     }     else     {         printf("Not Amicable");     } }

---
+++ Amicable_Numbers.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,i,sum1=0,sum2=0;
+    scanf("%d%d",&n,&m);
+    for(i=1;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            sum1=sum1+i;
+        }
+    }
+    for(i=1;i<m;i++)
+    {
+        if(m%i==0)
+        {
+            sum2=sum2+i;
+        }
+    }
+    if(sum1==m && sum2==n)
+    {
+        printf("Amicable");
+    }
+    else
+    {
+        printf("Not Amicable");
+    }
+}
