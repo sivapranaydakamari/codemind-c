a340ddcb9a1932625f8f41654eec9ac8383d5f43
sivapranaydakamari <147487910+sivapranaydakamari@users.noreply.github.com>
2023-12-07 23:22:07 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     for(int i=1;i<=n;i++)     {         for(int k=1;k<=i-1;k++)         {             printf(" ");         }         for(int j=1;j<=n-i+1;j++)         {             printf("%d ",n-i+1);         }         printf(" ");     } }

---
+++ Pattern_7.2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        for(int k=1;k<=i-1;k++)
+        {
+            printf(" ");
+        }
+        for(int j=1;j<=n-i+1;j++)
+        {
+            printf("%d ",n-i+1);
+        }
+        printf("
+");
+    }
+}
