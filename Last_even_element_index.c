f194fd2d57e591e5d01824b22691d7da212d77a6
sivapranaydakamari <147487910+sivapranaydakamari@users.noreply.github.com>
2023-12-19 15:29:11 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],s;     for(int i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(int i=0;i<n;i++)     {         if(a[i]%2==0)         {             s=i;         }     }     printf("%d",s); }

---
+++ Last_even_element_index.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],s;
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]%2==0)
+        {
+            s=i;
+        }
+    }
+    printf("%d",s);
+}
