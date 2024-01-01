09e5ddcef960f73acc01d11e7766144be5197dca
sivapranaydakamari <147487910+sivapranaydakamari@users.noreply.github.com>
2024-01-01 10:01:12 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int ar[n];     for(i=0;i<n;i++)     {         scanf("%d",&ar[i]);     }     int a,b,d=0;     scanf("%d%d",&a,&b);     for(i=0;i<n;i++)     {         if(ar[i]<a || ar[i]>b)         {             printf("%d ",ar[i]);             d++;         }     }     if(d==0)     printf("-1"); }

---
+++ Elements_not_in_between_A_and_B.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int ar[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    int a,b,d=0;
+    scanf("%d%d",&a,&b);
+    for(i=0;i<n;i++)
+    {
+        if(ar[i]<a || ar[i]>b)
+        {
+            printf("%d ",ar[i]);
+            d++;
+        }
+    }
+    if(d==0)
+    printf("-1");
+}
